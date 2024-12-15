3981415f466b8c44c7e91b9b981326931a8c2c2d
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-12-15 19:26:21 +0100

    Adjust App and Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { Outlet } from "react-router";
+import styled from "styled-components";
 //import "./App.css";
 
 import Header from "./components/Header";
+
+const Main = styled.main`
+    display: flex;
+    justify-content: center;
+`;
+
+const Content = styled.div`
+    max-width: 1000px;
+    width: 100%;
+`;
 
 function App() {
     return (
         <>
             <Header />
-            <Outlet />
+            <Main>
+                <Content>
+                    <Outlet />
+                </Content>
+            </Main>
         </>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,45 @@ import { Link } from "react-router";
 import styled from "styled-components";
 
 const HeaderContainer = styled.header`
+    position: sticky;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: grey;
+    height: 3.6rem;
+    top: 0;
+`;
+
+const HeaderContent = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 0.5rem 1rem;
-    background-color: grey;
+    padding: 0.25rem 1.25rem;
+    max-width: 1200px;
+    width: 100%;
+
+    @media (min-width: 768px) {
+        padding: 0.25rem 2.5rem;
+    }
 `;
 
 const LogoContainer = styled(Link)`
+    display: flex;
+    align-items: center;
     height: 2.5rem;
+    gap: 0.5rem;
+
+    @media (min-width: 768px) {
+        height: 2rem;
+    }
 `;
 
 const Logo = styled.img`
     height: 100%;
+`;
+
+const Title = styled.img`
+    height: 55%;
 `;
 
 const IconsContainer = styled.div`
@@ -25,7 +51,6 @@ const IconsContainer = styled.div`
 
 const SearchIcon = styled.img`
     height: 1.5rem;
-    margin-top: 0.25rem;
 `;
 
 const UserIcon = styled.img`
@@ -35,13 +60,18 @@ const UserIcon = styled.img`
 const Header = () => {
     return (
         <HeaderContainer>
-            <LogoContainer to="/">
-                <Logo src="/page-logo.svg" alt="GameVault" />
-            </LogoContainer>
-            <IconsContainer>
-                <SearchIcon src="/search-icon.svg" alt="Search" />
-                <UserIcon src="/user-icon.svg" alt="User" />
-            </IconsContainer>
+            <HeaderContent>
+                <LogoContainer to="/">
+                    <Logo src="/logo.svg" alt="GameVault" />
+                    <Title src="/title.svg" alt="GameVault" />
+                </LogoContainer>
+                <IconsContainer>
+                    <SearchIcon src="/search-icon.svg" alt="Search" />
+                    <Link to="/login">
+                        <UserIcon src="/user-icon.svg" alt="User" />
+                    </Link>
+                </IconsContainer>
+            </HeaderContent>
         </HeaderContainer>
     );
 };
